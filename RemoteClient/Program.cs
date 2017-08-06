48f86b04f497282d2e27964e1fe08f05a4d80271
rogeralsing <rogeralsing@gmail.com>
2017-08-06 17:18:55 +0200

    Starcounter + Proto.Actor - Remote demo

---
+++ RemoteClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Messages;
+using Microsoft.Extensions.Logging;
+using Proto;
+using Proto.Remote;
+
+namespace RemoteClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Serialization.RegisterFileDescriptor(Messages.ProtosReflection.Descriptor);
+
+            Log.SetLoggerFactory(new LoggerFactory()
+                .AddConsole(LogLevel.Debug));
+
+            Console.WriteLine("Starting");
+            Remote.Start("127.0.0.1", 0);
+            var pid = new PID("127.0.0.1:12000", "HelloActor");
+            Console.WriteLine("Requesting");
+            var res = pid.RequestAsync<HelloResponse>(new HelloRequest()).Result;
+            Console.WriteLine("Got response");
+            Console.WriteLine(res.Message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScServer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Messages;
+using Microsoft.Extensions.Logging;
+using Proto;
+using Proto.Remote;
+using Starcounter;
+using static Proto.Actor;
+
+namespace ScServer
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Log.SetLoggerFactory(new LoggerFactory()
+                .AddDebug(LogLevel.Debug));
+
+            Serialization.RegisterFileDescriptor(Messages.ProtosReflection.Descriptor);
+
+
+            Remote.Start("127.0.0.1", 12000);
+            var props = FromProducer(() => new HelloActor());
+            var helloPid = SpawnNamed(props, "HelloActor");
+
+            //REST to Actor comunication:
+            Handle.GET("/hello", () =>
+            {
+                //Blocking due to no async handlers in Starcounter
+                var resp = helloPid.RequestAsync<HelloResponse>(new HelloRequest()).Result;
+                return resp.Message;
+            });
+        }
+    }
+
+    [Database]
+    public class HelloState
+    {
+        public int HelloCount { get; set; }
+    }
+
+    public class HelloActor : IActor
+    {
+        public Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                case Started _:
+                    Trace.WriteLine("Hello Actor started");
+                    break;
+                case HelloRequest _:
+                    return Scheduling.RunTask(() =>
+                    {
+                        Trace.WriteLine($"Hello Actor got request from {context.Sender.ToShortString()}");
+                        Db.Transact(() =>
+                        {
+                            var state = Db.SQL<HelloState>("SELECT s from ScServer.HelloState s")
+                                            .FirstOrDefault() ?? new HelloState();
+
+                            state.HelloCount++;
+                            context.Respond(new HelloResponse
+                            {
+                                Message = $"You have said hello {state.HelloCount} times!"
+                            });
+                        });
+                    });
+            }
+            return Done;
+        }
+    }
+}
